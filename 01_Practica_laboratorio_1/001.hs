23eb6db3fa29d72de0e26ce2c7b59092c1c0df6c
kolodani <danielvinzia@gmail.com>
2023-03-22 16:03:58 -0300

    haskell, ejercicio 1a

---
+++ 01_Practica_laboratorio_1/001.hs
@@ -0,0 +1,46 @@
+{-
+1) Los siguientes códigos tienen errores, cargar el archivo 20.Practica.0.hs en el interprete de Haskell
+GHCi, leer los mensajes de error y corregirlos hasta que el archivo se cargue correctamente.
+-}
+
+-- a)
+{-
+regla b = case b of
+    True -> "Quedate en Casa"
+    False -> "Qudate en Casa"
+-}
+regla :: Bool -> [Char]
+regla b = if b then "Quedate en Casa" else "Qudate en casa"
+
+--------------------- POR RESOLVER -------------------------------------------------
+
+{-
+
+-- b)
+case [x]         =  []
+case (x:y:xs)      =  y : case (x:xs)
+case []          =  []
+
+-- c)
+map f []        =  []
+map f (x:xs)     =  f x : map f xs
+
+-- d)
+listNumeros = (1 : 2) : 'a' : []
+
+-- e)
+[]     ++! ys = ys
+(x:xs) ++! ys = x : xs ++! ys
+
+-- f)
+addToTail x xs = map +x tail xs
+
+-- g)
+listmin xs = head . sort xs
+
+-- h) (*)
+smap f [] = []
+smap f [x] = [f x]
+smap f (x:xs) = f x : smap (smap f) xs
+
+-}
