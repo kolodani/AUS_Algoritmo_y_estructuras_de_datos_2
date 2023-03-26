4b2f5e678318d51c1331284c6c47cd7ebcaba0e2
kolodani <danielvinzia@gmail.com>
2023-03-26 18:31:47 -0300

    haskell, ejercicio 2j

--- 01_Practica_laboratorio_1/002.hs
+++ 01_Practica_laboratorio_1/002.hs
@@ -54,13 +54,24 @@ xor a b
     |a == b = False
     |otherwise = True
 
+-- j) max3, que toma tres números enteros y devuelve el máximo entre ellos
+-- funcion que recibe tres parametros y devuelve 1
+max3 :: Ord a => a -> a -> a -> a
+max3 a b c
+    |a > b && a > c = a
+    |b > a && b > c = b
+    |otherwise = c
+
+-- funcion max2 saca el mayor y luego la uso para comparar el tercer valor
+max2 :: Ord p => p -> p -> p
+max2 a b = if a > b then a else b
+
+max3b :: Ord p => p -> p -> p -> p
+max3b a b c = if max2 a b > c then max a b else c
+
 
 ---------------------------- POR RESOLVER --------------------------------------
 
 {-
-
-
-j) max3, que toma tres números enteros y devuelve el máximo entre llos
-
 k) swap, que toma un par y devuelve el par con sus componentes invertidas
 -}
