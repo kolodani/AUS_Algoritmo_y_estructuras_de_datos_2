5815bf18fcce42e6afb823b609daa0c3cdc84713
kolodani <danielvinzia@gmail.com>
2023-03-26 16:01:37 -0300

    haskell, ejercicio 2a

---
+++ 01_Practica_laboratorio_1/002.hs
@@ -0,0 +1,35 @@
+{-
+2. Definir las siguientes funciones y determinar su tipo:
+-}
+
+--a) five, que dado cualquier valor, devuelve 5
+five :: Num p1 => p2 -> p1
+five a = 5
+
+
+
+---------------------------- POR RESOLVER --------------------------------------
+
+{-
+b) apply, que toma una función y un valor, y devuelve el resultado de
+aplicar la función al valor dado
+
+c) identidad, la función identidad
+
+d) first, que toma un par ordenado, y devuelve su primera componente
+
+e) derive, que aproxima la derivada de una función dada en un punto dado
+
+f) sign, la función signo
+
+g) vabs, la función valor absoluto (usando sign y sin usarla)
+
+h) pot, que toma un entero y un número, y devuelve el resultado de
+elevar el segundo a la potencia dada por el primero
+
+i) xor, el operador de disyunción exclusiva
+
+j) max3, que toma tres números enteros y devuelve el máximo entre llos
+
+k) swap, que toma un par y devuelve el par con sus componentes invertidas
+-}
