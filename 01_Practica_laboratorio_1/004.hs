89412c2dcece8d323cdc55845e718a8f724f2b69
kolodani <danielvinzia@gmail.com>
2023-03-28 17:27:27 -0300

    haskell, ejercicio 4a

---
+++ 01_Practica_laboratorio_1/004.hs
@@ -0,0 +1,34 @@
+{-
+4) Dar al menos dos ejemplos de funciones que tengan cada uno de los siguientes tipos:
+a) (Int -> Int) -> Int
+b) Int -> (Int -> Int)
+c) (Int -> Int) -> (Int -> Int)
+d) Int -> Bool
+e) Bool -> (Bool -> Bool)
+f) (Int,Char) -> Bool
+g) (Int,Int) -> Int
+h) Int -> (Int,Int)
+i) a -> Bool
+j) a -> a
+-}
+
+-- a) (Int -> Int) -> Int
+sumarA :: (Int, Int) -> Int
+sumarA (x , y) = x + y
+
+mayorA :: (Int, Int) -> Int
+mayorA (x , y)
+    |x > y = x
+    |otherwise = y
+
+{-
+b) Int -> (Int -> Int)
+c) (Int -> Int) -> (Int -> Int)
+d) Int -> Bool
+e) Bool -> (Bool -> Bool)
+f) (Int,Char) -> Bool
+g) (Int,Int) -> Int
+h) Int -> (Int,Int)
+i) a -> Bool
+j) a -> a
+-}
