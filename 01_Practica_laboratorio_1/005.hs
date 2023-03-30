a2bf97ad97943991bf8b7db4a2e1b3bce9245b65
kolodani <danielvinzia@gmail.com>
2023-03-30 13:41:33 -0300

    haskell, ejercicio 5b

--- 01_Practica_laboratorio_1/005.hs
+++ 01_Practica_laboratorio_1/005.hs
@@ -21,3 +21,19 @@ unique :: [Int] -> [Int]
 
 divisors :: Integral a => a -> [a]
 divisors x = [y | y <- [1..x], mod x y == 0]
+
+-- b) 'matches', que dados un entero 'x' y una lista de enteros descarta
+-- de la lista los elementos distintos a 'x'
+
+matches :: Int -> [Int] -> [Int]
+matches x xs = [y | y <- xs, y == x]
+
+{-
+c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
+'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
+donde 0 <= a, b, c, d <= 'n'
+
+(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
+'xs' sin elementos repetidos
+unique :: [Int] -> [Int]
+-}
