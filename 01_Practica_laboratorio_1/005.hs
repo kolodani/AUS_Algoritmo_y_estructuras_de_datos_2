409cfa870b8924a20e46c233b3b2cd903b057fd3
kolodani <danielvinzia@gmail.com>
2023-03-30 13:52:42 -0300

    haskell, ejercicio 5c

--- 01_Practica_laboratorio_1/005.hs
+++ 01_Practica_laboratorio_1/005.hs
@@ -21,18 +21,40 @@ unique :: [Int] -> [Int]
 
 divisors :: Integral a => a -> [a]
 divisors x = [y | y <- [1..x], mod x y == 0]
+{-
+    tomo todos los numeros de 1 a x y los divido por x, haciendo uso de la funcion mod
+    si el resto es 0, entonces es divisor de x y lo agrego a la lista de divisores
+    la expresion se lee como:
+    los y valores que estan entre 1 y x, que al dividirlos por x, el resto es 0
+-}
 
 -- b) 'matches', que dados un entero 'x' y una lista de enteros descarta
 -- de la lista los elementos distintos a 'x'
 
 matches :: Int -> [Int] -> [Int]
 matches x xs = [y | y <- xs, y == x]
+{-
+    tomo todos los elementos de la lista xs y los comparo con x
+    si son iguales, los agrego a la lista de matches
+    la expresion se lee como:
+    los y elementos de la lista xs, que son iguales a x
+-}
+
+-- c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
+-- '(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
+-- donde 0 <= a, b, c, d <= 'n'
+
+cuadrupla :: Int -> [(Int, Int, Int, Int)]
+cuadrupla n = [(a, b, c, d) | a <- [0..n], b <- [0..n], c <- [0..n], d <- [0..n], a^2 + b^2 == c^2 + d^2]
+{-
+    tomo todos los valores de a, b, c y d que estan entre 0 y n
+    y los comparo con la expresion a^2 + b^2 == c^2 + d^2
+    si es verdadero, entonces agrego la cuadrupla a la lista de cuadruplas
+    la expresion se lee como:
+    las cuadruplas (a, b, c, d) que estan entre 0 y n, que cumplen la expresion a^2 + b^2 == c^2 + d^2
+-}
 
 {-
-c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
-'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
-donde 0 <= a, b, c, d <= 'n'
-
 (d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
 'xs' sin elementos repetidos
 unique :: [Int] -> [Int]
