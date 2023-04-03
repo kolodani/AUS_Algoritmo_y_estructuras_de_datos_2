6d555c5bf7911bc6a2be047ccba72b329f9bd6a2
kolodani <danielvinzia@gmail.com>
2023-04-03 09:40:11 -0300

    haskell, ejercicio 7d

--- 01_Practica_laboratorio_1/007.hs
+++ 01_Practica_laboratorio_1/007.hs
@@ -22,15 +22,14 @@ todos :: [Bool] -> Bool
 todos [] = True
 todos (x:xs) = x && todos xs
 
-
+-- d) 'codes', que dada una lista de caracteres, devuelve la
+-- lista de sus ordinales
+codes :: [Char] -> [Int]
+codes [] = []
+codes (x:xs) = (fromEnum x) : codes xs
 
 
 {-
-
-
-
-d) 'codes', que dada una lista de caracteres, devuelve la
-lista de sus ordinales
 
 e) 'restos', que calcula la lista de los restos de la
 división de los elementos de una lista de números dada por otro
