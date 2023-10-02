33ca6df163eb20c9486c2699dfd358bde88d2ea2
Kolodani <danielvinzia@gmail.com>
2023-10-02 17:23:22 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.1.3

---
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.1.3.hs
@@ -0,0 +1,8 @@
+{-
+    1.1.3
+    Definir una funcion para calcular el area de un circulo, dado su radio r
+    (usando 22/7, como aproximacion de pi)
+-}
+
+circulo :: Int -> Int
+circulo r = (22 `div` 7) * (r * r)
