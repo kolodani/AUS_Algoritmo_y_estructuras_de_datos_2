ca2227920fd3e5089e2d2468c1105fda0d4f10b7
Kolodani <danielvinzia@gmail.com>
2023-10-02 17:25:18 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.4.4

---
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.4.4.hs
@@ -0,0 +1,8 @@
+{-
+    1.4.4
+    Dar un tipo apropiado para la integral definida de una funcion en analisis
+    matematico, tal como se usa en la frase "la ingregal de f de a a b"
+-}
+
+integral :: (Float -> Float) -> Float -> Float -> Float
+integral f a b = undefined
