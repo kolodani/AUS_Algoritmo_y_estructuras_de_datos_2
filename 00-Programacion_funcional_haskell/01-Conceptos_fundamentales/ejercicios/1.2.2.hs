011e551415d5fc744dddc117e174d66a6d778c0a
Kolodani <danielvinzia@gmail.com>
2023-10-02 17:23:47 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.2.2

---
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.2.2.hs
@@ -0,0 +1,15 @@
+{-
+    1.2.2
+    ¿cuantas secuencias de reduccion existen para la expresion cuadrado (3 + 4)?
+-}
+
+-- cuadrado (3 + 4)
+-- cuadrado (7)
+-- 7 * 7
+-- 49
+
+-- cuadrado (3 + 4)
+-- (3 + 4) * (3 + 4)
+-- (3 + 4) * 7
+-- 7 * 7
+-- 49
