2f8f06892145365cd6f3e46ab819b8b560be009b
Kolodani <danielvinzia@gmail.com>
2023-10-03 15:37:03 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.4.6

---
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.4.6.hs
@@ -0,0 +1,12 @@
+{-
+    1.4.6
+    ¿Cual de las siguientes definicione, caso de serlo alguna es correcta?
+    (x)x = (xx)
+    (+)x = (x+)
+    (-)x = (-x)
+-}
+
+-- Respuesta
+-- (x)x = (xx) es correcta
+-- (+)x = (x+) es correcta
+-- (-)x = (-x) es correcta
