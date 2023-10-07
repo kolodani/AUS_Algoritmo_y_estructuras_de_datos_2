bce88c5a748ea9103238c3b563ef3d551e0c9831
Kolodani <danielvinzia@gmail.com>
2023-10-07 14:22:04 -0300

    haskell, programacion funcional haskell, tipo de datos simples, 2.1.6

---
+++ 00-Programacion_funcional_haskell/02-Tipo_de_datos_simples/ejercicios/2.1.6.hs
@@ -0,0 +1,9 @@
+{-
+    2.1.6
+    Definir una funcion Ordena3 que ordene tres enteros en orden creciente.
+    A partir de ella, definir una funcion, llamemosla triangulo' , que no
+    dependa de la suposicion de que sus argumentos estan en orden no decreciente.
+-}
+
+Ordena3 :: (Int, Int, Int) -> (Int, Int, Int)
+Ordena3 (x, y, z) = 
