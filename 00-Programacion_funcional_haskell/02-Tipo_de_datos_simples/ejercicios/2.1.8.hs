b6d2b5e6a48f27ae77838c1348ae856831dd84b0
Kolodani <danielvinzia@gmail.com>
2023-10-09 10:35:49 -0300

    haskell, programacion funcional haskell, tipo de datos simples, 2.1.8

---
+++ 00-Programacion_funcional_haskell/02-Tipo_de_datos_simples/ejercicios/2.1.8.hs
@@ -0,0 +1,7 @@
+{-
+    2.1.8
+    ¿Existen numeros que puedan ser comparados mediante (==) pero que no puedan
+    serlo razonablemente mediante (<)?
+-}
+
+-- No, ya que si se puede comparar con (==) se puede comparar con (<) y viceversa.
