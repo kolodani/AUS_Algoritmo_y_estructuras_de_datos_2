2c2dda6eabf414008ad78078ace87388694c87c8
Kolodani <danielvinzia@gmail.com>
2023-10-09 10:36:11 -0300

    haskell, programacion funcional haskell, tipo de datos simples, 2.1.10

---
+++ 00-Programacion_funcional_haskell/02-Tipo_de_datos_simples/ejercicios/2.1.10.hs
@@ -0,0 +1,8 @@
+{-
+    2.1.10
+    ¿Que propiedades esperariamos que cumpliera (<) en cualquier declaracion de
+    concrecion?
+-}
+
+-- (<) :: Ord a => a -> a -> Bool
+-- x < y = x <= y && x /= y
