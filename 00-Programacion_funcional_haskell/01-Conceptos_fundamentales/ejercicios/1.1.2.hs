c8186b06e65de99641ad9004d930694a50157390
kolodani <danielvinzia@gmail.com>
2023-11-13 11:24:04 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.1.2

--- 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.1.2.hs
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.1.2.hs
@@ -5,3 +5,11 @@
 
 mayor :: Integer -> Integer -> Integer
 mayor a b = if a >= b then a else b
+
+mayorB :: (Integer, Integer) -> Integer
+mayorB (a, b) = if a >= b then a else b
+
+mayorC :: Integer -> Integer -> Integer
+mayorC a b
+    | a >= b    = a
+    | otherwise = b
