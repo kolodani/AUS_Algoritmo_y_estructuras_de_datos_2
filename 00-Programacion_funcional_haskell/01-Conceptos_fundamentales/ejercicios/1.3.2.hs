7f692b782e6ca1fbe1422453797cb007a884075c
kolodani <danielvinzia@gmail.com>
2023-11-13 11:24:37 -0300

    haskell, programacion funcional haskell, conceptos fundamentales, 1.3.2

--- 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.3.2.hs
+++ 00-Programacion_funcional_haskell/01-Conceptos_fundamentales/ejercicios/1.3.2.hs
@@ -1,7 +1,7 @@
 {-
     1.3.2
     Supongase que definimos la funcion h mediante la ecuacion h x = f (g x).
-    mostrar que si tanto f como g son extrictas, entonces tambien h lo es.
+    mostrar que si tanto f como g son estrictas, entonces tambien h lo es.
 -}
 
 -- h x = f (g x)
@@ -9,5 +9,5 @@
 -- que devuelve la composicion de f y g evaluadas en x.
 -- si f y g son extrictas, entonces h tambien lo es.
 -- ahora esto sucede porque cada vez que haga h x, primero evaluara g x y luego
--- f (g x). si g x es extricta, entonces evaluara f (g x) y si f (g x) es
--- extricta, entonces h x es extricta.
+-- f (g x). si g x es estricta, entonces evaluara f (g x) y si f (g x) es
+-- estricta, entonces h x es estricta.
