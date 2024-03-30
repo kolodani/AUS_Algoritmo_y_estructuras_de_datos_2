db737f735eaadb33a42a8224e98c039b050c9473
kolodani <danielvinzia@gmail.com>
2024-03-30 19:03:16 -0300

    Haskell, practica de laboratorio, 005

---
+++ 001-Practica_de_laboratorio/005-ejercicio.hs
@@ -0,0 +1,18 @@
+{-
+5) Definir las siguientes funciones usando listas por comprensión:
+-}
+{-
+a) 'divisors', que dado un entero positivo 'x' devuelve la
+lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
+
+b) 'matches', que dados un entero 'x' y una lista de enteros descarta
+de la lista los elementos distintos a 'x'
+
+c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
+'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
+donde 0 <= a, b, c, d <= 'n'
+
+(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
+'xs' sin elementos repetidos
+unique :: [Int] -> [Int]
+-}
