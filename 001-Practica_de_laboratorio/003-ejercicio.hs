69ed4cc9daf70acd6b0636247bbeaee93d0e1554
kolodani <danielvinzia@gmail.com>
2024-03-30 19:02:46 -0300

    Haskell, practica de laboratorio, 003

---
+++ 001-Practica_de_laboratorio/003-ejercicio.hs
@@ -0,0 +1,71 @@
+{-
+3) Definir una función que determine si un año es bisiesto o no, de
+acuerdo a la siguiente definición:
+
+año bisiesto 1. m. El que tiene un día más que el año común, añadido al mes de febrero. Se repite
+cada cuatro años, a excepción del último de cada siglo cuyo número de centenas no sea múltiplo
+de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
+
+¿Cuál es el tipo de la función definida?
+-}
+
+bisiesto :: (Integral a) => a -> Bool
+bisiesto x =
+  if x `mod` 4 == 0
+    then
+      if x `mod` 100 == 0
+        then
+          if x `mod` 400 == 0
+            then True
+            else False
+        else True
+    else False
+
+bisiesto' :: (Integral a) => a -> [Char]
+bisiesto' x =
+  if x `mod` 4 == 0
+    then
+      ( if x `mod` 100 == 0
+          then
+            ( if x `mod` 400 == 0
+                then "es bisiesto"
+                else "no es bisiesto"
+            )
+          else "es bisiesto"
+      )
+    else "no es bisiesto"
+
+-- segunda manera
+modulo4 :: (Integral a) => a -> Bool
+modulo4 x
+  | mod x 4 == 0 = True
+  | otherwise = False
+
+modulo100 :: (Integral a) => a -> Bool
+modulo100 x
+  | mod x 100 == 0 = True
+  | otherwise = False
+
+modulo400 :: (Integral a) => a -> Bool
+modulo400 x
+  | mod x 400 == 0 = True
+  | otherwise = False
+
+bisiesto2 :: (Integral a) => a -> [Char]
+bisiesto2 x
+  | not (modulo4 x) = "no es bisiesto"
+  | modulo4 x && not (modulo100 x) = "es bisiesto"
+  | modulo4 x && modulo100 x && not (modulo400 x) = "no es bisiesto"
+  | modulo4 x && modulo100 x && modulo400 x = "es bisiesto"
+  | otherwise = "no es bisiesto"
+
+-- mejorando la logica del segundo intento
+bisiesto3 :: (Integral a) => a -> [Char]
+bisiesto3 x
+  | modulo4 x && not (modulo100 x) = "es bisiesto"
+  | modulo4 x && modulo100 x && modulo400 x = "es bisiesto"
+  | otherwise = "no es bisiesto"
+
+-- resolucion del profesor
+kk :: (Integral a) => a -> Bool
+kk a = (mod a 400 == 0) || (mod a 4 == 0) && not (mod a 100 == 0)
