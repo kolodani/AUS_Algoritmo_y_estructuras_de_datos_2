4db812ed8e765ec046da2f0eab5b2b8d9e0a0406
kolodani <danielvinzia@gmail.com>
2024-03-30 21:07:21 -0300

    Haskell, practica de laboratorio, 006

--- 001-Practica_de_laboratorio/005-ejercicio.hs
+++ 001-Practica_de_laboratorio/005-ejercicio.hs
@@ -1,18 +1,27 @@
 {-
 5) Definir las siguientes funciones usando listas por comprensión:
 -}
-{-
-a) 'divisors', que dado un entero positivo 'x' devuelve la
-lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
 
-b) 'matches', que dados un entero 'x' y una lista de enteros descarta
-de la lista los elementos distintos a 'x'
+-- a) 'divisors', que dado un entero positivo 'x' devuelve la
+-- lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
+divisors :: (Integral a) => a -> [a]
+divisors n = [x | x <- [1 .. n], n `mod` x == 0]
 
-c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
-'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
-donde 0 <= a, b, c, d <= 'n'
+-- b) 'matches', que dados un entero 'x' y una lista de enteros descarta
+-- de la lista los elementos distintos a 'x'
+matches :: Int -> [Int] -> [Int]
+matches x ys = [y | y <- ys, y == x]
 
-(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
-'xs' sin elementos repetidos
+-- c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
+-- '(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
+-- donde 0 <= a, b, c, d <= 'n'
+cuadrupla :: Int -> [(Int,Int,Int,Int)]
+cuadrupla n = [(a, b, c, d) | a <- [0 .. n], b <- [0 .. n], c <- [0 .. n], d <- [0 .. n], a ^ 2 + b ^ 2 == c ^ 2 + d ^ 2]
+
+-- (d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
+-- 'xs' sin elementos repetidos
+-- unique :: [Int] -> [Int]
 unique :: [Int] -> [Int]
--}
+unique [] = []
+unique [x] = [x]
+unique (x:xs) = x : unique [y | y <- xs, y /= x]
