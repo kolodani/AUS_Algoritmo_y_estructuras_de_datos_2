8fb66d0a1021d00b22c9bafc52047396ff4e225a
netbook XFCE kolo <danielvinzia@gmail.com>
2024-09-17 10:29:14 -0300

    repaso, 001 practica de laboratorio, 102

--- 001-Practica_de_laboratorio/100-repaso/102-PLR.hs
+++ 001-Practica_de_laboratorio/100-repaso/102-PLR.hs
@@ -19,26 +19,50 @@ identidad :: a -> a
 identidad x = x
 
 --d) first, que toma un par ordenado, y devuelve su primera componente
+first :: (a , a) -> a
+first (x , y) = x
 
+--e) derive, que aproxima la derivada de una función dada en un punto dado
+derive :: (Fractional a, Fractional t) => (t -> a) -> t -> a
+derive f x = (f (x + 0.00001) - f x) / 0.00001
 
+--f) sign, la función signo
+sign :: (Ord a1, Num a1, Num a2) => a1 -> a2
+sign x
+    | x > 0 = 1
+    | x < 0 = -1
+    | otherwise = 0
 
+--g) vabs, la función valor absoluto (usando sign y sin usarla)
+vabsSin :: (Ord a, Num a) => a -> a
+vabsSin x
+    |x < 0 = -x
+    |otherwise = x
 
+vabsCon :: (Ord a, Num a) => a -> a
+vabsCon x
+    |sign x == (-1) = -x
+    |otherwise = x
 
+--h) pot, que toma un entero y un número, y devuelve el resultado de
+--elevar el segundo a la potencia dada por el primero
+pot :: Int -> Integer -> Integer
+pot x y = y ^ x
 
+--i) xor, el operador de disyunción exclusiva
+xor :: (Eq a) => a -> a -> Bool
+xor x y
+    |x == y = False
+    |otherwise = True
 
-{-
-e) derive, que aproxima la derivada de una función dada en un punto dado
+--j) max3, que toma tres números enteros y devuelve el máximo entre ellos
+max3:: Int -> Int -> Int -> Int
+max3 x y z
+    |x > y && x > z = x
+    |y > x && y > z = y
+    |otherwise = z
 
-f) sign, la función signo
+--k) swap, que toma un par y devuelve el par con sus componentes invertidas
+swap :: (Int, Int) -> (Int, Int)
+swap (x, y) = (y, x)
 
-g) vabs, la función valor absoluto (usando sign y sin usarla)
-
-h) pot, que toma un entero y un número, y devuelve el resultado de
-elevar el segundo a la potencia dada por el primero
-
-i) xor, el operador de disyunción exclusiva
-
-j) max3, que toma tres números enteros y devuelve el máximo entre llos
-
-k) swap, que toma un par y devuelve el par con sus componentes invertidas
--}
