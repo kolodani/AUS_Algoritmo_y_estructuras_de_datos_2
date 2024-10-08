aaf9c192cf990bb319c5f49abbe610cfe94c55dc
kolodani <danielvinzia@gmail.com>
2024-10-08 15:54:47 -0300

    Empezando desde cero

---
+++ 200-Aprende_haskell_por_el_bien_de_todos/001-Empecemos/001-preparados_listos_ya.hs
@@ -0,0 +1,45 @@
+-- Operaciones comunes en haskell
+-- Suma
+sumaDosNumeros = 2 + 4
+
+-- Producto
+productoDosNumeros = 4 * 5
+
+-- Resta
+restaDosNumeros = 1892 - 1472
+
+-- Division
+divisionDosNumeros = 5 / 2
+
+-- Operaciones mas simples en haskell
+operacion1 = (50 * 100) - 4999
+operacion2 = 50 * 100 - 4999
+operacion3 = 50 * (100 - 4999)
+
+-- Observacion: Para los numeros negativos, siempre van entre parentesis
+negativos = 5 * (-3)
+
+-- tambien contamos con algebra booleana
+-- Y es &&
+yBooleano1 = True && False
+yBooleano2 = True && True
+
+-- O es ||
+oBooleano1 = False || True
+oBooleano2 = False || False
+
+-- Contamos con comprobacion de igualdad
+-- Igual
+iguales1 = 5 == 5
+iguales2 = 1 == 0
+
+-- Diferentes
+diferentes1 = 5 /= 5
+diferentes2 = 5 /= 4
+
+-- Tambien con palabras
+comparandoPalabras1 = "Hola" == "Hola"
+comparandoPalabras2 = "Hola" == "hola"
+
+-- suma entre entero y flotante
+enteroFlotante = 5 + 4.5
