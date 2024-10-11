07b2dba60d875bae95805c9c91d8176f78d1b707
kolodani <danielvinzia@gmail.com>
2024-10-11 16:36:37 -0300

    variables de tipo

---
+++ 200-Aprende_haskell_por_el_bien_de_todos/002-tipos_y_clases_de_tipos/002-variables_de_tipo.hs
@@ -0,0 +1,5 @@
+{-
+    podemos aprovechar el interprete de haskell y preguntarle el tipo
+    de a una variable o a una funcion, con :t esto es muy util si creamos
+    una funcion y no sabemos como tiparla
+-}
