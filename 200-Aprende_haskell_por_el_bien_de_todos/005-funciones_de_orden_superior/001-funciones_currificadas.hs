6303ac6d1308fa8a76d02c21bc3e03d1eefbb116
kolodani <danielvinzia@gmail.com>
2024-10-22 16:49:38 -0300

    funcciones currificadas

---
+++ 200-Aprende_haskell_por_el_bien_de_todos/005-funciones_de_orden_superior/001-funciones_currificadas.hs
@@ -0,0 +1,14 @@
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred2 :: (Num a, Ord a) => a -> Ordering
+compareWithHundred2 = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
