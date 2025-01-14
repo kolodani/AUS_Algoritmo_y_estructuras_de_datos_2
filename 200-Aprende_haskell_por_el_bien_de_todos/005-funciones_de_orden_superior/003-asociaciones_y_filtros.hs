565a9cd231e8a9f4d0cf7f7827d7d1c849aa76f2
kolodani <danielvinzia@gmail.com>
2025-01-14 10:56:10 -0300

    Aprenda Haskell, funciones de orden superior, asociaciones y filtros

--- 200-Aprende_haskell_por_el_bien_de_todos/005-funciones_de_orden_superior/003-asociaciones_y_filtros.hs
+++ 200-Aprende_haskell_por_el_bien_de_todos/005-funciones_de_orden_superior/003-asociaciones_y_filtros.hs
@@ -19,6 +19,23 @@ largestDivisible :: (Integral a) => a
 largestDivisible = head (filter' p [100000,99999..])
     where p x = x `mod` 3829 == 0
 
+sumatoriaCuadrados :: Integer
+sumatoriaCuadrados = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
 
+sumatoriaCuadrados2 :: Integer
+sumatoriaCuadrados2 = sum (takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
 
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1 .. 100]))
+  where
+    isLong xs = length xs > 15
+
+listOfFuns :: [Integer -> Integer]
+listOfFuns = map (*) [0..]
 
