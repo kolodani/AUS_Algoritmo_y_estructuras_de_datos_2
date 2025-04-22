0a2302a51971a6745efea13c27fb986d92f388a9
kolodani <danielvinzia@gmail.com>
2025-04-22 16:06:11 -0300

    repasando para el examen 103

--- 001-Practica_de_laboratorio/100-repaso/103-PLR.hs
+++ 001-Practica_de_laboratorio/100-repaso/103-PLR.hs
@@ -1,6 +1,7 @@
 module Practica0 where
 
 import Data.List
+
 {-
 3) Definir una función que determine si un año es bisiesto o no, de
 acuerdo a la siguiente definición:
@@ -12,4 +13,8 @@ de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
 ¿Cuál es el tipo de la función definida?
 -}
 
-
+bisiesto x
+  | mod x 400 == 0 = "Bisiesto"
+  | mod x 100 == 0 = "No Bisiesto"
+  | mod x 4 == 0 = "Bisiesto"
+  | otherwise = "No Bisiesto"
