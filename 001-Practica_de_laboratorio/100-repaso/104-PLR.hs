e35ba2b85b359609add8d258954487e411ec1594
kolodani <danielvinzia@gmail.com>
2025-04-22 20:10:08 -0300

    repaso para el examen 104

--- 001-Practica_de_laboratorio/100-repaso/104-PLR.hs
+++ 001-Practica_de_laboratorio/100-repaso/104-PLR.hs
@@ -1,83 +1,3 @@
-module Practica0 where
-
-import Data.List
-
-{-
-1) Los siguientes códigos tienen errores, cargar el archivo 20.Practica.0.hs en el interprete de Haskell
-GHCi, leer los mensajes de error y corregirlos hasta que el archivo se cargue correctamente.
--}
-
--- a)
-regla b = case b of
-    True -> "Quedate en Casa"
-    False -> "Qudate en Casa"
-
--- b)
-case [x]         =  []
-case (x:y:xs)      =  y : case (x:xs)
-case []          =  []
-
--- c)
-map f []        =  []
-map f (x:xs)     =  f x : map f xs
-
--- d)
-listNumeros = (1 : 2) : 'a' : []
-
--- e)
-[]     ++! ys = ys
-(x:xs) ++! ys = x : xs ++! ys
-
--- f)
-addToTail x xs = map +x tail xs
-
--- g)
-listmin xs = head . sort xs
-
--- h) (*)
-smap f [] = []
-smap f [x] = [f x]
-smap f (x:xs) = f x : smap (smap f) xs
-
-{-
-2. Definir las siguientes funciones y determinar su tipo:
-
-a) five, que dado cualquier valor, devuelve 5
-
-b) apply, que toma una función y un valor, y devuelve el resultado de
-aplicar la función al valor dado
-
-c) identidad, la función identidad
-
-d) first, que toma un par ordenado, y devuelve su primera componente
-
-e) derive, que aproxima la derivada de una función dada en un punto dado
-
-f) sign, la función signo
-
-g) vabs, la función valor absoluto (usando sign y sin usarla)
-
-h) pot, que toma un entero y un número, y devuelve el resultado de
-elevar el segundo a la potencia dada por el primero
-
-i) xor, el operador de disyunción exclusiva
-
-j) max3, que toma tres números enteros y devuelve el máximo entre llos
-
-k) swap, que toma un par y devuelve el par con sus componentes invertidas
--}
-
-{-
-3) Definir una función que determine si un año es bisiesto o no, de
-acuerdo a la siguiente definición:
-
-año bisiesto 1. m. El que tiene un día más que el año común, añadido al mes de febrero. Se repite
-cada cuatro años, a excepción del último de cada siglo cuyo número de centenas no sea múltiplo
-de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
-
-¿Cuál es el tipo de la función definida?
--}
-
 {-
 4) Dar al menos dos ejemplos de funciones que tengan cada uno de los siguientes tipos:
 a) (Int -> Int) -> Int
@@ -92,78 +12,107 @@ i) a -> Bool
 j) a -> a
 -}
 
+-- a) (Int -> Int) -> Int
+-- la funcion que se esta definiendo toma como parametro una funcion que recibe un entero y devuelve un entero y por
+-- ultimo devuelve un entero
+-- funcion auxiliar para usar en el ejemplo a
+auxiliarA :: Int -> Int
+auxiliarA x = x + 1
 
-{-
-5) Definir las siguientes funciones usando listas por comprensión:
+sumar3A :: (Int -> Int) -> Int
+sumar3A f = f 3
 
-a) 'divisors', que dado un entero positivo 'x' devuelve la
-lista de los divisores de 'x' (o la lista vacía si el entero no es positivo)
+-- funcion auxiliar para usar en el ejemplo a
+opuestoA :: Int -> Int
+opuestoA x = -x
 
-b) 'matches', que dados un entero 'x' y una lista de enteros descarta
-de la lista los elementos distintos a 'x'
+valorAbsolutoA :: (Int -> Int) -> Int
+valorAbsolutoA f = f (-3)
 
-c) 'cuadrupla', que dado un entero 'n', devuelve todas las cuadruplas
-'(a,b,c,d)' que satisfacen a^2 + b^2 = c^2 + d^2,
-donde 0 <= a, b, c, d <= 'n'
+-- b) Int -> (Int -> Int)
+-- la funcion que se esta definiendo toma como parametro un entero y devuelve una funcion que recibe un entero y
+-- devuelve un entero
+addB :: Int -> (Int -> Int)
+addB x y = x + y
 
-(d) 'unique', que dada una lista 'xs' de enteros, devuelve la lista
-'xs' sin elementos repetidos
-unique :: [Int] -> [Int]
--}
+productoB :: Int -> (Int -> Int)
+productoB x y = x * y
 
+-- c) (Int -> Int) -> (Int -> Int)
+-- la funcion que se esta definiendo toma como parametro una funcion que recibe un entero y devuelve un entero y
+-- devuelve una funcion que recibe un entero y devuelve un entero
+-- funcion auxiliar para usar en el ejemplo c
+auxiliarC :: Int -> Int
+auxiliarC x = x * 2
 
+ejemploC :: (Int -> Int) -> (Int -> Int)
+ejemploC f x = f (f x)
 
-{-
-6) El producto escalar de dos listas de enteros de igual longitud
-es la suma de los productos de los elementos sucesivos (misma
-posición) de ambas listas.  Definir una función 'scalarProduct' que
-devuelva el producto escalar de dos listas.
+-- funcion auxiliar para usar en el ejemplo c
+potenciaC :: Int -> Int
+potenciaC x = x * x
 
-Sugerencia: Usar las funciones 'zip' y 'sum'. -}
+ejemC :: (Int -> Int) -> (Int -> Int)
+ejemC f x = f (f x) * 3
 
-{-
-7) Sin usar funciones definidas en el
-preludio, defina recursivamente las siguientes funciones y
-determine su tipo más general:
+-- d) Int -> Bool
+-- la funcion que se esta definiendo toma como parametro un entero y devuelve un booleano
+esParD :: Int -> Bool
+esParD x = if x `mod` 2 == 0 then True else False
 
-a) 'suma', que suma todos los elementos de una lista de números
+esPositivoD :: Int -> Bool
+esPositivoD x = if x > 0 then True else False
 
-b) 'alguno', que devuelve True si algún elemento de una
-lista de valores booleanos es True, y False en caso
-contrario
+-- e) Bool -> (Bool -> Bool)
+-- la funcion que se esta definiendo toma como parametro un booleano y devuelve una funcion que recibe un booleano y
+-- devuelve un booleano
+andE :: Bool -> (Bool -> Bool)
+andE x y = x && y
 
-c) 'todos', que devuelve True si todos los elementos de
-una lista de valores booleanos son True, y False en caso
-contrario
+orE :: Bool -> (Bool -> Bool)
+orE x y = x || y
 
-d) 'codes', que dada una lista de caracteres, devuelve la
-lista de sus ordinales
+-- f) (Int,Char) -> Bool
+-- la funcion que se esta definiendo toma como parametro una tupla de un entero y un caracter y devuelve un booleano
+ejemF :: (Int, Char) -> Bool
+ejemF (x, y)
+  | x == 10 && y == 'a' = True
+  | otherwise = False
 
-e) 'restos', que calcula la lista de los restos de la
-división de los elementos de una lista de números dada por otro
-número dado
+turnosF :: (Int, Char) -> Bool
+turnosF (x, y)
+  | x > 7 && x < 19 && y /= 'D' = True
+  | otherwise = False
 
-f) 'cuadrados', que dada una lista de números, devuelva la
-lista de sus cuadrados
+-- g) (Int,Int) -> Int
+-- la funcion que se esta definiendo toma como parametro una tupla de dos enteros y devuelve un entero
+sumaG :: (Int, Int) -> Int
+sumaG (x, y) = x + y
 
-g) 'longitudes', que dada una lista de listas, devuelve la
-lista de sus longitudes
+potenG :: (Int, Int) -> Int
+potenG (x, y) = x ^ y
 
-h) 'orden', que dada una lista de pares de números, devuelve
-la lista de aquellos pares en los que la primera componente es
-menor que el triple de la segunda
+-- h) Int -> (Int,Int)
+-- la fincion que se esta definiendo toma como parametro una tupla de dos enteros y devuelve un entero
+alPlanoH :: Int -> (Int, Int)
+alPlanoH x = (x, 2 * x)
 
-i) 'pares', que dada una lista de enteros, devuelve la lista
-de los elementos pares
+alCuboH :: Int -> (Int, Int)
+alCuboH x = (x, x ^ 3)
 
-j) 'letras', que dada una lista de caracteres, devuelve la
-lista de aquellos que son letras (minúsculas o mayúsculas)
+-- i) a -> Bool
+-- la funcion que se esta definiendo toma como parametro un valor de cualquier tipo y devuelve un booleano
+esVerdadI :: a -> Bool
+esVerdadI x = True
 
-k) 'masDe', que dada una lista de listas 'xss' y un
-número 'n', devuelve la lista de aquellas listas de 'xss'
-con longitud mayor que 'n' -}
+esFalsoI :: a -> Bool
+esFalsoI x = False
 
-{-
-8) Redefinir las funciones del ejercicio anterior usando foldr, map y filter.
-ver su definición en https://hoogle.haskell.org/
--}
+-- j) a -> a
+-- la funcion que se esta definiendo toma como parametro un valor de cualquier tipo y devuelve un valor de cualquier
+-- tipo
+identidadJ :: a -> a
+identidadJ x = x
+
+ejemJ :: a -> a
+ejemJ x = x
